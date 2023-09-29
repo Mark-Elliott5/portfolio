397555fa83582750c11521fd162545b67527c249
Mark Elliott <115184056+Mark-Elliott5@users.noreply.github.com>
2023-09-28 22:56:29 -0400

    Updated background animation and prettier rules

--- src/index.js
+++ src/index.js
@@ -19,41 +19,32 @@ anime({
   easing: 'spring(1, 30, 20, 0)',
 });
 
-const projects = document.getElementsByClassName('project');
+function addPerspectiveWarping() {
+  const projects = document.getElementsByClassName('project');
+  for (let i = 0; i < projects.length; i += 1) {
+    projects[i].addEventListener('mousemove', (e) => {
+      const boundingBox = projects[i].getBoundingClientRect();
+      const centerX = boundingBox.left + boundingBox.width / 2;
+      const centerY = boundingBox.top + boundingBox.height / 2;
 
-for (let i = 0; i < projects.length; i += 1) {
-  projects[i].addEventListener('mousemove', (e) => {
-    const boundingBox = projects[i].getBoundingClientRect();
-    const centerX = boundingBox.left + boundingBox.width / 2;
-    const centerY = boundingBox.top + boundingBox.height / 2;
+      const mouseX = e.clientX - centerX;
+      const mouseY = e.clientY - centerY;
 
-    const mouseX = e.clientX - centerX;
-    const mouseY = e.clientY - centerY;
+      const tiltX = (mouseY / centerY) * 10; // Adjust the tilt sensitivity
+      const tiltY = -(mouseX / centerX) * 10; // Adjust the tilt sensitivity
 
-    const tiltX = (mouseY / centerY) * 10; // Adjust the tilt sensitivity
-    const tiltY = -(mouseX / centerX) * 10; // Adjust the tilt sensitivity
-
-    projects[
-      i
-    ].style.transform = `perspective(400px) rotateX(${tiltX}deg) rotateY(${tiltY}deg)`;
-  });
-  projects[i].addEventListener('mouseleave', () => {
-    projects[i].style.transform =
-      'perspective(400px) rotateX(0deg) rotateY(0deg)';
-  });
+      projects[
+        i
+      ].style.transform = `perspective(400px) rotateX(${tiltX}deg) rotateY(${tiltY}deg)`;
+    });
+    projects[i].addEventListener('mouseleave', () => {
+      projects[i].style.transform =
+        'perspective(400px) rotateX(0deg) rotateY(0deg)';
+    });
+  }
 }
 
-// function generateBinaryString() {
-//   const length = window.innerWidth;
-//   let string = '';
-//   for (let i = 0; i < length; i += 1) {
-//     const randomInt = Math.floor(Math.random() * 2);
-//     string += randomInt;
-//   }
-//   return string;
-// }
-
-function newGenerateBinaryString() {
+function generateBinaryString() {
   const spaces = Math.floor(window.innerWidth / 4.8);
   const length = spaces / 4;
   let string = '';
@@ -67,13 +58,14 @@ function newGenerateBinaryString() {
   const remainingSpaceLength = spaces - newStringLength;
   let newString = '';
   const repeatedString = ` `.repeat(remainingSpaceLength);
-  newString += string.slice(0, stringMidpoint);
-  newString += repeatedString;
-  newString += string.slice(stringMidpoint);
+  newString +=
+    string.slice(0, stringMidpoint) +
+    repeatedString +
+    string.slice(stringMidpoint);
   return newString;
 }
 
-const colorsClasses = ['green-color', 'amber-color'];
+const colorsClasses = ['amber-color', 'green-color'];
 
 const blinkingUnderscore = document.getElementById('blinking-underscore');
 blinkingUnderscore.addEventListener('click', () => {
@@ -84,7 +76,7 @@ blinkingUnderscore.addEventListener('click', () => {
   colorsClasses.push(currentColor);
 });
 
-function updateBackground() {
+function generateBackground() {
   const svg = document.getElementById('background-svg');
   svg.replaceChildren();
   const width = window.innerWidth;
@@ -97,17 +89,36 @@ function updateBackground() {
       'http://www.w3.org/2000/svg',
       'text'
     );
-    textElem.setAttribute('x', '1');
+    textElem.setAttribute('x', '0');
     textElem.setAttribute('y', i);
     textElem.setAttribute(
       'style',
       `font-size: 8px; white-space: pre; text-wrap: nowrap; fill: var(--main-color); white-space: pre;`
     );
-    textElem.textContent = newGenerateBinaryString();
+    textElem.textContent = generateBinaryString();
     svg.append(textElem);
   }
-  const intervalTime = 500;
+}
+
+function updateBackground() {
+  const textElems = Array.from(
+    document.getElementsByTagNameNS('http://www.w3.org/2000/svg', 'text')
+  );
+  const prevValue = textElems[0].getAttribute('y');
+  for (let i = 0; i < textElems.length; i += 1) {
+    const nextElem = i + 1;
+    if (nextElem === textElems.length) {
+      textElems[i].setAttribute('y', prevValue);
+      break;
+    }
+    textElems[i].setAttribute('y', textElems[nextElem].getAttribute('y'));
+  }
+  const intervalTime = 50;
   setTimeout(updateBackground, intervalTime);
 }
 
+addPerspectiveWarping();
+generateBackground();
+
+window.addEventListener('resize', generateBackground);
 updateBackground();
